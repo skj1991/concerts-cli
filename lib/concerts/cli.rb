b316c96b792e1f71c18407d536bd2a1db9a553d6
Stacy Johns <stacykjohns@icloud.com>
2019-11-22 17:41:52 +0000

    first commit

---
+++ lib/concerts/cli.rb
@@ -0,0 +1,8 @@
+require "concerts/cli/version"
+
+module Concerts
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/concerts/cli/version.rb
@@ -0,0 +1,5 @@
+module Concerts
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
